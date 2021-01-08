31ecbcb84d146b301fc5248ae413132552ec0c21
Anderson <andersoninfonet@gmail.com>
2021-01-08 01:59:43 -0300

    💄add StepHeader to Orders and Routes

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg'
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -9,16 +10,16 @@ const Home = () => {
             <div className="home-container">
                 <div className="home-content">
                     <div className="home-actions">
-                        <h1 className="home-title">
-                            Faça seu pedido <br /> que entregamos <br /> para você !!!
+                    <h1 className="home-title">
+                        Faça seu pedido <br /> que entregamos <br /> para você !!!
                     </h1>
-                        <h3 className="home-subtitle">
-                            Escolha seu pedido e em poucos minutos <br />
+                    <h3 className="home-subtitle">
+                        Escolha seu pedido e em poucos minutos <br />
                         levaremos na sua porta.
                     </h3>
-                        <a href="orders" className="home-btn-order">
-                            FAZER PEDIDO
-                    </a>
+                    <Link to="orders" className="home-btn-order">
+                        FAZER PEDIDO
+                    </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
---
+++ src/Routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Navbar from './Navbar';
+import Orders from "./Orders";
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Switch>
+                <Route path="/orders"> 
+                    <Orders />
+                </Route>
+                <Route path="/" exact={true}>
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
